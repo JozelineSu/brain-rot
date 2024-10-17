10ef344494f7eef584f13601a17de315e9ea7f18
unknown <jozelinesuarez@gmail.com>
2024-10-17 16:06:46 -0500

    discover page styling

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,5 +1,5 @@
 import NavBar from "../components/NavBar";
-
+import "../styles/Discover.css";
 
 function Discover() {
 
@@ -7,7 +7,33 @@ function Discover() {
     return (
         
         <div className="discover">
-           <NavBar/> 
+           <NavBar/>
+           <div className="searchbar">
+            Search
+           </div>
+
+           <div className="posts">{/*list of all posts*/}
+                <div className="user-post">{/*how one post will look */}
+                    <div className="character-pic">
+                        Character Pic
+                    </div>     
+
+                    <div className="post-content">
+                        <div className="post-text">
+                            This is my post tralking about my charactrer
+                        </div>
+                        <div className="post-buttons">
+                            <button>Like</button>
+                            <button>Comment</button>
+                            <button>Repost</button>
+
+                        </div>
+                        <div className="tags">
+                            <p>#tag, #tag, #tag, # tag</p>
+                        </div>                        
+                    </div>
+                </div>
+           </div> 
             
         </div>
     )
